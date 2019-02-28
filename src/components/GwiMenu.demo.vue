d475800ea2173c46b2884be4ab54899655f264f2
gwildu <gwildu@gwildu.ch>
2019-02-28 06:21:58 +0100

    add basic demo case for menu

---
+++ src/components/GwiMenu.demo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <GwiDemoCase name="only items props">
+      <GwiMenu :items="dummyItems" />
+    </GwiDemoCase>
+  </div>
+</template>
+
+<script>
+import GwiMenu from 'gwi-vue-components/GwiMenu/GwiMenu'
+import GwiDemoCase from '../tooling/GwiDemoCase'
+
+export default {
+  name: 'GwiMenuDemo',
+  components: {GwiMenu, GwiDemoCase},
+  data () {
+    return {
+      dummyItems: [
+        {
+          displayName: 'Home',
+          url: '/home'
+        },
+        {
+          displayName: 'Some other page',
+          url: '/something',
+          items: [
+            {
+              displayName: 'Something else',
+              url: '/something/else'
+            },
+            {
+              displayName: 'Something completely different',
+              url: '/something/different'
+            }
+          ]
+        },
+        {
+          displayName: 'Some other page',
+          url: '/something',
+          items: [
+            {
+              displayName: 'Something else',
+              url: '/something/else'
+            },
+            {
+              displayName: 'Something completely different',
+              url: '/something/different'
+            }
+          ]
+        },
+        {
+          displayName: 'Last',
+          url: '/last'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
